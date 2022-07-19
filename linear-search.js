d86777de8d3bec6a01794301fc61bdd297e1e9d8
William Lucht <aaronlucht@gmail.com>
2022-07-19 14:23:32 -0700

    added algo

---
+++ linear-search.js
@@ -0,0 +1,17 @@
+// Write code to create a function that accepts an array of numbers and a target number
+// if the target number is present in the array, return its index
+// else return -1
+// You may not use the `indexOf`, `lastIndexOf` or `includes` methods
+
+var linearSearch = function(arr, target) {
+    for (var i =0; i <arr.length; i++){
+        let currentNumber = arr[i];
+
+        if(currentNumber === target){
+            return i;
+        }
+    }
+    return -1;
+};
+
+// const linearSearch = (arr, target)=> arr.reduce((a,b,i)=>( b===target?i:a),-1)
